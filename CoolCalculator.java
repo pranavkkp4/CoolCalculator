b855480fca6043da83263a047977ab00de1b5729
Pranavkp <36557877+pranavkkp4@users.noreply.github.com>
2023-03-08 09:40:40 -0500

    Added Prime Checker and Modified the Methods
    Added a method that checks if the two numbers entered are prime. I also changed the methods so that they are all void methods now and moved to the print statements to the methods so that the methods can now just be called, and don't have to print the text in front of them

--- CoolCalculator.java
+++ CoolCalculator.java
@@ -7,20 +7,58 @@ public class CoolCalculator {
         this.a = a;
         this.b = b;
     }
-    public double add(){
-        return this.a + this.b;
+    public void add(){
+        double sum = this.a+this.b;
+        System.out.println("The sum of " + this.a + " and "+ this.b + sum);
     }
-    public double subtract(){
-        return this.a-this.b;
+    public void subtract(){
+        double difference = this.a-this.b;
+        System.out.println("The difference between " + this.a + "and " + this.b + difference);
     }
-    public double multiply(){
-        return this.a*this.b;
+    public void multiply(){
+        double product = this.a*this.b;
+        System.out.println("The product of "+this.a + "and " + this.b + " is " +product);
     }
-    public double divide(){
-        return this.a/this.b;
+    public void divide(){
+        double quotient = this.a/this.b;
+        System.out.println("The quotient of "+ this.a + "divide by "+this.b+ " is " + quotient);
     }
-    public double modulo(){
-        return this.a%this.b;
+    public void modulo(){
+        double remainder = this.a%this.b;
+        System.out.println("The remainder of "+ this.a + "divided by "+this.b+ " is " + remainder);
+    }
+    public void isPrime(){
+        boolean aStatus = true;
+        boolean bStatus = true;
+        //if number is 2, then is prime, if less than 2 can't be prime
+        if(this.a<2){
+            aStatus = false;
+        }
+        if(this.b<2){
+            bStatus = false;
+        }
+        for(int i = 2; i < this.a; i++){
+            if(this.a%i==0){
+                aStatus = false;
+            }
+        }
+        for(int i = 2; i < this.b; i++){
+            if(this.b%i==0){
+                bStatus = false;
+            }
+        }
+        if(aStatus==true){
+            System.out.println("The number "+this.a+" is prime");
+        }
+        else{
+            System.out.println("The number "+this.a+" is not prime");
+        }
+        if(bStatus==true){
+            System.out.println("The number"+this.b+"is prime");
+        }
+        else{
+            System.out.println("The number "+this.b+ "is not prime");
+        }
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -31,25 +69,32 @@ public class CoolCalculator {
         double num2 = input.nextDouble();
         CoolCalculator myCalculator = new CoolCalculator(num1,num2);
         System.out.println("Enter the number that matches the operation you want to do ");
-        System.out.println("1. Addition \n 2. Subtraction \n 3.Multiplication 4. Division \n 5. Modulo");
+        System.out.println("1. Addition \n 2. Subtraction \n 3.Multiplication 4. Division \n 5. Modulo \n +" +
+                "6. Prime Check");
         int choice = input.nextInt();
         System.out.println("Your numbers were " + myCalculator.a+ " and "+ myCalculator.b);
         if(choice==1||choice==2||choice==3||choice==4||choice==5||choice==6||choice==7||choice==8){
             if(choice==1){
-                System.out.println("The sum of your numbers is "+myCalculator.add());
+                myCalculator.add();
             }
             else if(choice==2){
-                System.out.println("The difference of your numbers is "+myCalculator.subtract());
+                myCalculator.subtract();
             }
             else if(choice==3){
-                System.out.println("The product of your numbers is "+myCalculator.multiply());
+                myCalculator.multiply();
             }
             else if(choice==4){
-                System.out.println("The quotient of your numbers is "+myCalculator.divide());
+                myCalculator.divide();
             }
             else if(choice==5){
-                System.out.println("The remainder of your numbers is "+myCalculator.modulo());
+                myCalculator.modulo();
             }
+            else if(choice==6){
+                myCalculator.isPrime();
+            }
+        }
+        else{
+            System.out.println("Invalid Input");
         }
     }
 }
